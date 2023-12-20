91b52b1a98f873603a2046eaf74177628161d347
nnasri2 <naoufalnasri750@gmail.com>
2023-12-20 14:24:34 -0500

    header + footer + css +bootstrap work

---
+++ includs/footer.php
@@ -0,0 +1,61 @@
+<!-- Footer -->
+<footer class="text-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-lg-4 col-xl-3">
+                <h5>About</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <p class="mb-0 text-dark">
+                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+                </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto">
+                <h5>Informations</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><a href="">Link 1</a></li>
+                    <li><a href="">Link 2</a></li>
+                    <li><a href="">Link 3</a></li>
+                    <li><a href="">Link 4</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+                <h5>Others links</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><a href="">Link 1</a></li>
+                    <li><a href="">Link 2</a></li>
+                    <li><a href="">Link 3</a></li>
+                    <li><a href="">Link 4</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3">
+                <h5>Contact</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-home mr-2"></i> My company</li>
+                    <li><i class="fa fa-envelope mr-2"></i> email@example.com</li>
+                    <li><i class="fa fa-phone mr-2"></i> + 33 12 14 15 16</li>
+                    <li><i class="fa fa-print mr-2"></i> + 33 12 14 15 16</li>
+                </ul>
+            </div>
+            <div class="col-12 copyright mt-3">
+                <p class="float-left">
+                    <a href="#">Back to top</a>
+                </p>
+                <p class="text-right text-muted">Commerce électronique 1 <i class="fa fa-heart"></i> by <a href="#"><i>Naoufal</i></a> | <span>v. 1.0</span></p>
+            </div>
+        </div>
+    </div>
+</footer> 	
+ 	
+ 	
+ 	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+ 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ includs/header-super.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+	// Check if user is not logged in
+	if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'super') {
+		session_destroy();
+		header('Location: ../login.php');
+		exit();
+	}
+
+
+//Logout function
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header('Location: /index.php');
+    exit();
+}
+
+function displayErrorMessage($message)
+{
+    echo '<div class="error">' . $message . '</div>';
+}
+
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ECOM : SUPER DASHBOARD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../styles/super_dashboard.css">
+  </head>
+  <body>
+<!-- HTML content for client dashboard -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container ">
+        <a class="navbar-brand" href="/index.php">ECOM Store</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="/carte.php">Cart</a>
+                </li> -->
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+              
+ 
+                <?php 
+
+					if (isset($_SESSION['user_id']) ) {?>
+                		<a class="btn btn-warning btn-sm ml-4" href="?logout=true">
+                     		Logout
+                    	</a>
+                        <?php if (isset($_SESSION['role']) == 'super' ) {?>
+                            <a class="btn btn-success" href="/super/dashboard.php">
+                                <i class="fa fa-home"></i> Dashboard</a></a>
+                        <?php }?>
+                    
+                    <?php }else{ ?>
+                		<a class="btn btn-primary btn-sm ml-4" href="/login.php">
+                    	<i class="fa fa-user"></i>Login</a>
+                    <?php } ?>
+
+                     
+            </div>
+
+        </div>
+    </div>
+</nav>
+
+
